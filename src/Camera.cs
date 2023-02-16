476f2f7922d86c8f29a5f59249d2935ea5efd6dc
ashish <sam4lordjesus@outlook.com>
2023-02-16 12:38:13 +0530

    Made camera's viewport editable

--- src/Camera.cs
+++ src/Camera.cs
@@ -15,24 +15,24 @@ public class Camera2D
 	/// <value>Rotation angle in radians</value>
 	public float Rotation { get; set; }
 	private Matrix _transform;
-	private readonly GraphicsDevice _graphicsDevice;
+	/// <summary>The current Viewport of the screen. Should be changed only when the screen resolution/fullscreen setting is changed</summary>
+	public Rectangle Viewport;
 	/// <summary>Camera constructor</summary>
-	/// <param name="graphicsDevice">The graphicsdevice to obtain the world coordinates from</param>
-	public Camera2D(GraphicsDevice graphicsDevice)
+	/// <param name="screen">The viewport to obtain the world coordinates from</param>
+	public Camera2D(Rectangle screen)
 	{
-		_graphicsDevice = graphicsDevice;
 		Zoom = 1f;
 		Rotation = 0;
 		Position = new(0, 0);
+		Viewport = screen;
 	}
 	/// <summary>With the given values, update the transform matrix for the camera</summary>
 	public void Update()
 	{
-		var viewport = _graphicsDevice.Viewport;
 		_transform = Matrix.CreateTranslation(new Vector3(-Position.X, -Position.Y, 0)) *
 					 Matrix.CreateRotationZ(Rotation) *
 					 Matrix.CreateScale(new Vector3(Zoom, Zoom, 1)) *
-					 Matrix.CreateTranslation(new Vector3(viewport.Width * 0.5f, viewport.Height * 0.5f, 0));
+					 Matrix.CreateTranslation(new Vector3(Viewport.Width * 0.5f, Viewport.Height * 0.5f, 0));
 	}
 	/// <summary>Returns the transform matrix for the camera</summary>
 	/// <returns>Transfrom matrix</returns>
