4b4b028ede8faeec678f31db9ae5e3f2122a43f8
ashish <sam4lordjesus@outlook.com>
2023-06-24 16:55:35 +0530

    dividing into multiple interfaces

--- src/IGameStage.cs
+++ src/IGameStage.cs
@@ -3,11 +3,11 @@ using Microsoft.Xna.Framework;
 namespace Azuxiren.MG;
 
 /// <summary>
-/// Represents a stage within the game
+/// Represents a simple component within the game
 /// </summary>
 /// <typeparam name="Parameters">Constant Parameter Type shared for the game</typeparam>
 /// <typeparam name="Settings">Variable Setting Type shared between screens of the game</typeparam>
-public interface IGameStage<Parameters, Settings>
+public interface IGameLoadableComponent<Parameters, Settings>
 {
 	/// <summary>
 	/// Loads the required content. This is a required 
@@ -16,6 +16,33 @@ public interface IGameStage<Parameters, Settings>
 	/// <param name="parameters">The object that contain all constant paramters for the game</param>
 	/// <param name="settings">The shared settings for the game</param>
 	public void LoadContent(in Parameters parameters, ref Settings settings);
+}
+
+/// <summary>
+/// Represents a drawable component within the game
+/// </summary>
+/// <typeparam name="Parameters">Constant Parameter Type shared for the game</typeparam>
+/// <typeparam name="Settings">Variable Setting Type shared between screens of the game</typeparam>
+public interface IDrawableComponent<Parameters, Settings>
+{
+	/// <summary>
+	/// The logic for drawing the components within the game. 
+	/// It is not supposed to alter any existing setting
+	/// </summary>
+	/// <param name="gt">The GameTime object for this frame of the game</param>
+	/// <param name="parameters">The object that contain all constant paramters for the game</param>
+	/// <param name="settings">The shared settings for the game</param>
+	public void Draw(GameTime gt, in Parameters parameters, in Settings settings);
+}
+
+
+/// <summary>
+/// Represents a stage within the game
+/// </summary>
+/// <typeparam name="Parameters">Constant Parameter Type shared for the game</typeparam>
+/// <typeparam name="Settings">Variable Setting Type shared between screens of the game</typeparam>
+public interface IGameStage<Parameters, Settings> : IGameLoadableComponent<Parameters, Settings>, IDrawableComponent<Parameters, Settings>
+{
 	/// <summary>
 	/// The logic for updating the components within the game. 
 	/// It may alter any existing setting. <br/>
@@ -31,12 +58,4 @@ public interface IGameStage<Parameters, Settings>
 	/// <param name="nextStage">The nextStage for the game, if any. Otherwise null is returned</param>
 	/// <returns>true the screen needs to be updated, or game needs to be exited/closed. Otherwise false</returns>
 	public bool Update(GameTime gt, in Parameters parameters, ref Settings settings, out IGameStage<Parameters, Settings>? nextStage);
-	/// <summary>
-	/// The logic for drawing the components within the game. 
-	/// It is not supposed to alter any existing setting
-	/// </summary>
-	/// <param name="gt">The GameTime object for this frame of the game</param>
-	/// <param name="parameters">The object that contain all constant paramters for the game</param>
-	/// <param name="settings">The shared settings for the game</param>
-	public void Draw(GameTime gt, in Parameters parameters, in Settings settings);
 }
